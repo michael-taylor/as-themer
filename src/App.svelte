016ad6b02482f6a356a53ed641f2529286f2c6dd
Michael R. Taylor <michael@atomic.city>
2025-10-07 01:02:43 -0500

    Added Editorconfig and standardized formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
-  import CodePreview from './lib/CodePreview.svelte'
+	import CodePreview from "./lib/CodePreview.svelte";
 </script>
 
-<div class="text-center p-8">
-  <h1 class="h1">
-    Automation Studio Color Themer
-  </h1>
-  <div class="container">
-    <CodePreview />
-  </div>
+<div class="text-center p-8 space-y-4">
+	<h1 class="h1">Automation Studio Color Themer</h1>
+	<div class="container">
+		<CodePreview />
+	</div>
 </div>
 
 <style>
-  .container {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    column-gap: 30px;
-  }
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		column-gap: 30px;
+	}
 </style>
