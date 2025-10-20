555c046a8169b314e5e7c95744de75d497dd7724
Michael R. Taylor <michael@atomic.city>
2025-10-20 02:09:42 -0500

    Huge improvement to the C source code themer. It's still fairly naive, though.

--- src/components/c-themer.ts
+++ src/components/c-themer.ts
@@ -1,35 +1,126 @@
-import type {Theme} from "./theme.ts";
+import type {Theme} from "./themes.ts";
+
+function readTokens(line: string) : string[] {
+    let tokenBuffer = "";
+    let tokens: string[] = [];
+    let inString = false;
+    let inComment = false;
+    let inNumber = false;
+
+    [...line].forEach((c) => {
+        if (inNumber) {
+            if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(c)) {
+                tokenBuffer += c;
+            } else {
+                tokens.push(tokenBuffer);
+                inNumber = false;
+                tokenBuffer = c;
+            }
+        } else if (inComment) {
+            tokenBuffer += c;
+        } else if (inString) {
+            tokenBuffer += c;
+            if (c === "\"" || c === "'") {
+                tokens.push(tokenBuffer);
+                inString = false;
+                tokenBuffer = "";
+            }
+        } else if (c === "\"" || c === "'") {
+            inString = true;
+            tokenBuffer += c;
+        } else if (c === " ") {
+            tokens.push(tokenBuffer);
+            tokenBuffer = "";
+            tokens.push(c);
+        } else if (['(', ')', '+', '-', '*', '=', '{', '}', '[', ']', ',', ';'].includes(c)) {
+            tokens.push(tokenBuffer);
+            tokenBuffer = "";
+            tokens.push(c);
+        } else if (c == "/") {
+            inComment = true;
+            tokenBuffer = c;
+        } else if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(c)) {
+            tokens.push(tokenBuffer);
+            inNumber = true;
+            tokenBuffer = c;
+        } else {
+            tokenBuffer += c;
+        }
+    });
+
+    tokens.push(tokenBuffer);
+
+    return tokens;
+}
+
+function isNumber(s: string) : boolean {
+    return !isNaN(parseInt(s));
+}
+
+function isKeyword(s: string) : boolean {
+    return ["void", "char", "int", "for", "sizeof", "return"].includes(s);
+}
+
+function isDatatype(s: string) : boolean {
+    return ["UDINT", "DINT", "UINT", "INT", "STRING"].includes(s);
+}
+
+function isString(s: string) : boolean {
+    return s.startsWith("\"") || s.startsWith("'");
+}
+
+function isComment(s: string) : boolean {
+    return s.startsWith("//") || s.startsWith("/*");
+}
+
+function isOperator(s: string) : boolean {
+    return ['(', ')', '+', '-', '*', '=', '{', '}', '[', ']', ',', ';'].includes(s);
+}
 
 export function themeC(code: string, theme: Theme) : string[] {
     // Perform theming (WARNING: very naive algorithm)
-    const keywords = ["void", "char", "int", "for", "sizeof", "return"] as const;
-    const datatypes = ["UDINT", "DINT", "UINT", "INT", "STRING"] as const;
 
     let lines = code.split("\n");
 
     for (let i = 0; i < lines.length; i++) {
-        // We're assuming multi-line comments are not going to be multi-line in this sample code.
-        if ((lines[i]?.trim().startsWith("//")) || (lines[i]?.trim().startsWith("/*") && lines[i]?.trim().endsWith("*/"))) {
-            lines[i] = `<span style="color: ${theme.comments.foreground}">${lines[i]}</span>`;
-        } else if (lines[i]?.trim().startsWith("#include")) {
-            lines[i] = lines[i]?.replace("#include", `<span style="color: ${theme.keywords.foreground}">#include</span>`) ?? "";
-            let re = RegExp("(&lt;.+&gt;)", "g");
-            lines[i] = lines[i]?.replace(re, `<span style="color: ${theme.includeFiles.foreground}">$1</span>`) ?? "";
+        let line = lines[i] ?? "";
+
+        if (line.trim().startsWith("#include")) {
+            let re = /(&lt;.+&gt;)/g;
+            // Replace the angle brackets for HTML
+            line = line.replace("<", "&lt;").replace(">", "&gt;");
+            line = line.replace(re, `<span style="color: ${theme.includeFiles()}">$1</span>`);
+            line = line.replace("#include", `<span style="color: ${theme.keywords()}">#include</span>`);
         } else {
-            let stringRegExp = RegExp("(\".+\")", "g");
-            lines[i] = lines[i]?.replace(stringRegExp, `<span style="color: ${theme.strings.foreground}">$1</span>`) ?? "";
-            keywords.forEach((value) => {
-                let re = RegExp(value, "g");
-                lines[i] = lines[i]?.replace(re, `<span style="color: ${theme.keywords.foreground}">${value}</span>`) ?? "";
-            });
-            datatypes.forEach((value) => {
-                let re = RegExp(value, "g");
-                lines[i] = lines[i]?.replace(re, `<span style="color: ${theme.datatypes.foreground}">${value}</span>`) ?? "";
-            });
-            // TODO: Need to figure out how to add '=', '<', and '>' without messing up HTML
-            let operatorRegExp = /([\(\)\+\-\*{}\[\];,])/g;
-            lines[i] = lines[i]?.replace(operatorRegExp, `<span style="color: ${theme.operators.foreground}">$1</span>`) ?? "";
+            var tokens = readTokens(line);
+            var newLine = "";
+
+            tokens.forEach((t) => {
+                if (t == "") {
+
+                } else if (t == " ") {
+                    newLine += " ";
+                } else if (isNumber(t)) {
+                    newLine += `<span style="color: ${theme.numbers()}">${t}</span>`;
+                } else if (isKeyword(t)) {
+                    newLine += `<span style="color: ${theme.keywords()}">${t}</span>`;
+                } else if (isDatatype(t)) {
+                    newLine += `<span style="color: ${theme.datatypes()}">${t}</span>`;
+                } else if (isString(t)) {
+                    newLine += `<span style="color: ${theme.strings()}">${t}</span>`;
+                } else if (isComment(t)) {
+                    newLine += `<span style="color: ${theme.comments()}">${t}</span>`;
+                } else if (isOperator(t)) {
+                    newLine += `<span style="color: ${theme.operators()}">${t}</span>`;
+                } else {
+                    newLine += `<span style="color: ${theme.foreground}">${t}</span>`;
+                }
+            })
+
+            line = newLine + "\n";
         }
+
+        lines[i] = line;
     }
 
     return lines;
