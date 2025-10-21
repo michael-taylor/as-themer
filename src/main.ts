d3312e2fd149e5045ea4195b9fcb3f01cbe3513a
Michael R. Taylor <michael@atomic.city>
2025-10-20 23:55:40 -0500

    Added auto dark mode.

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,9 @@ import App from './App.vue'
 const vuetify = createVuetify({
     components,
     directives,
+    theme: {
+        defaultTheme: 'system',
+    },
 })
 
 createApp(App).use(vuetify).mount('#app')
