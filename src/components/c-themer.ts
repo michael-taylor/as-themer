a16ad5f1830b3130a720b08fda6126bfc07ca31c
Michael R. Taylor <michael@atomic.city>
2025-10-24 22:31:37 -0500

    Fixed highlighting of hex numbers.

--- src/components/c-themer.ts
+++ src/components/c-themer.ts
@@ -9,7 +9,7 @@ function readTokens(line: string) : string[] {
 
     [...line].forEach((c) => {
         if (inNumber) {
-            if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(c)) {
+            if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', 'X'].includes(c)) {
                 tokenBuffer += c;
             } else {
                 tokens.push(tokenBuffer);
