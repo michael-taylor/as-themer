c1fa1ae73bf9f31e22ceb153259e647b35da9a01
Michael R. Taylor <michael@atomic.city>
2025-10-24 23:26:55 -0500

    Setup Vite for Github pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import ViteFonts from 'unplugin-fonts/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      ViteFonts({
-          fontsource: {
-              families: [
-                  {
-                      name: 'Roboto',
-                      weights: [100, 300, 400, 500, 700, 900],
-                      styles: ['normal', 'italic'],
-                  },
-              ],
-          },
-      }),
-      vue()
-  ],
+    base: '/as-themer/',
+    plugins: [
+        ViteFonts({
+            fontsource: {
+                families: [
+                    {
+                        name: 'Roboto',
+                        weights: [100, 300, 400, 500, 700, 900],
+                        styles: ['normal', 'italic'],
+                    },
+                ],
+            },
+        }),
+        vue()
+    ],
 })
